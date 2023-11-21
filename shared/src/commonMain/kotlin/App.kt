f9bdae6fd02e751be5ad63102d75422e0f1cb2a1
Pire <p.clement01@orange.fr>
2023-11-21 15:21:42 +0100

    Add background

--- shared/src/commonMain/kotlin/App.kt
+++ shared/src/commonMain/kotlin/App.kt
@@ -26,47 +26,7 @@ private val repository = QuizRepository()
 fun App() {
 
     MaterialTheme {
-
-        //scoreScreen("10/10")
-        /*var questions = listOf(
-            Question(
-                1,
-                "Android is a great platform ?",
-                1,
-                listOf(Answer(1, "YES"), Answer(2, "NO"))
-            ),
-            Question(
-                1,
-                "Android is a bad platform ?",
-                2,
-                listOf(Answer(1, "YES"), Answer(2, "NO"))
-            )
-        )*/
-
         rootNavHost()
-
-        /*val questions = repository.questionState.collectAsState()
-
-        if(questions.value.isNotEmpty()) {
-            questionScreen(questions.value)
-        }*/
-
-
-        /*var greetingText by remember { mutableStateOf("Hello, World!") }
-        var showImage by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = {
-                greetingText = "Hello, ${getPlatformName()}"
-                showImage = !showImage
-            }) {
-                Text(greetingText)
-            }
-            AnimatedVisibility(showImage) {
-                Image(
-                    painterResource("compose-multiplatform.xml"),
-                    contentDescription = "Compose Multiplatform icon"
-                )
-            }*/
         }
     }
 
